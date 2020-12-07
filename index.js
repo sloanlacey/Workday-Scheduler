715ca31e35ee57a2f7b8321d6a98a7e99c9d5744
sloanlacey <sloanlacey89@gmail.com>
2020-12-07 15:41:28 -0800

    Display date and time on main page, rows rendered.

---
+++ index.js
@@ -0,0 +1,7 @@
+  var NowDate = new Date();
+  
+  // display value of moment object in #displayMoment div
+  
+  // display value of Date object in #displayJsDate div
+  var eDisplayDate = document.getElementById('currentDay');
+  eDisplayDate.innerHTML = NowDate;
