9ccce82838679b993e94b23229c5f4911db0977f
sloanlacey <sloanlacey89@gmail.com>
2020-12-07 17:45:08 -0800

    Render rows through JS and trim HTML.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
   var NowDate = new Date();
-  
-  // display value of moment object in #displayMoment div
-  
-  // display value of Date object in #displayJsDate div
+//   moment().format('MMMM Do YYYY, h:mm:ss a');
   var eDisplayDate = document.getElementById('currentDay');
   eDisplayDate.innerHTML = NowDate;
+
+// Render Rows
+  var container = $('.container');
+  var hourArray = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+
+  for (i = 0; i < hourArray.length; i++) {
+    var row = $('<div>').addClass('row time-block');
+    container.append(row);
+    var hourDisplay = $('<div>').addClass('col-1 hour');
+    hourDisplay.text(hourArray[i]); 
+    var textArea = $('<textarea>').addClass('col-10 description time-block');
+    var saveBtn = $('<button>').addClass('saveBtn col-1');
+    row.append(hourDisplay, textArea, saveBtn);
+    var saveIcon = $('<i>').addClass('far fa-save');
+    saveBtn.append(saveIcon);
+  }
