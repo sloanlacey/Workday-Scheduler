3ac525d41a228ea1df0b1d8b553c00ff24a83c61
sloanlacey <sloanlacey89@gmail.com>
2020-12-11 14:21:48 -0800

    Rework the save to local storage function, adjust event listener for save buttons so that it calls the save to local storage function, add ID's to the rows via the twenty four hour time array integers, each hour slot is now saving properly to local storage.

--- index.js
+++ index.js
@@ -3,35 +3,38 @@
   var nowDate = moment().format('MMMM Do YYYY, h:mm:ss a');
   var displayDate = document.getElementById('currentDay');
   displayDate.innerHTML = nowDate;
-//   Variables
+//   Variables for time
   var m = moment();
   var nowTime = m.format('LTS');
   var nowHour = newDate.getHours();
 
-// Hours array
+// Hour arrays for time display and for loops
+// Reference container in HTML
   var container = $('.container');
   var businessHours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
   var twentyFourHourTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
+// Call Functions
   renderRows();
-  saveEnteredEvents();
 
-function saveEnteredEvents() {
-  $('.saveBtn').click (function () {
-    // console.log($(this).siblings('textarea').val());
-    const enterEvent = $(this).attr('enter-event');
-    const key = `Event:${enterEvent}`;
+
+
+// Event listener for clicking any of the save buttons--it calls the function for storing the task list
+  $('.saveBtn').click(dailyTasks);
+// Function for saving daily tasks in day planner
+   function dailyTasks() {
+    // console.log('I have been clicked!');
+    const key = $(this).attr('id');
     const value = $(this).siblings('textarea').val();
-    localStorage.setItem(key, value);
-    }) 
-}
+    localStorage.setItem(key, JSON.stringify(value));
+   }
+
 
 // Render Rows
   function renderRows() {
   for (i = 0; i < twentyFourHourTime.length; i++) {
     var row = $('<div>');
     row.addClass('row time-block');
-// Add id's to rows that iterate through array
+// Add id's to rows that iterate through array of rows
     $(row).attr('id', (i + 9) + '-hour');
     container.append(row);
 // Render business hours on left side display
@@ -39,43 +42,26 @@ function saveEnteredEvents() {
     var hourDisplay = $('<div>').addClass('col-1 hour');
     hourDisplay.text(businessHours[i]);    
    }
+
 // Row colors
-    if (nowHour < twentyFourHourTime[i]) {
+    if (nowHour > twentyFourHourTime[i]) {
         row.addClass('past');
     }
-    else if (nowHour > twentyFourHourTime[i]) {
+    else if (nowHour < twentyFourHourTime[i]) {
         row.addClass('future');
   }
     else if (nowHour == twentyFourHourTime[i]) {
         row.addClass('present');
     }
 // Render text boxes and save buttons with a save icon
-    var textArea = $('<textarea>').addClass('col-10 description time-block');
-    textArea.attr('id', 'enter-event');
-    var saveBtn = $('<button>').addClass('saveBtn col-1');
-    saveBtn.attr('id', 'save-button');
+    var textArea = $('<textarea>').addClass('col-10 description time-block textarea').attr('id', twentyFourHourTime[i]);
+    var saveBtn = $('<button>').addClass('saveBtn col-1').attr('id', twentyFourHourTime[i]);
     row.append(hourDisplay, textArea, saveBtn);
     var saveIcon = $('<i>').addClass('far fa-save');
     saveBtn.append(saveIcon);  
 }
   }
 
-
-// var enterInfo = document.getElementById('enter-event');
-// var saveInfo = document.getElementsByClassName('saveBtn');
-
-// // Event Listeners
-// $('.saveBtn').click(function() {
-//     $(this).parent();
-//     localStorage.setItem('Event', enterInfo.value);
-
-
-//     function getEvents() {
-//         var events = localStorage.getItem('Event');
-//         $('#enter-event').text(events);
-//     }
-//     getEvents();
-// })
 
 
 
